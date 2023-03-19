fbfe8a30f6462b56707fa08679df79c47b41b3c7
Animashaun Taofiq <animashauntaofiq@yahoo.com>
2023-03-19 22:14:50 +0100

    refactor: added reusable components

---
+++ src/components/reusables/BaseInput.vue
@@ -0,0 +1,57 @@
+<template>
+  <!-- Base Input (text, email, password) -->
+  <div>
+    <label class="label-style" :for="label" v-if="label">{{ label }}</label>
+    <div class="relative">
+      <input
+        :id="label"
+        :type="type"
+        :value="modelValue"
+        :placeholder="placeholder"
+        v-bind="$attrs"
+        @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+        class="input-style"
+        :class="{ err: error }"
+      />
+      <Icon
+        v-if="password"
+        class="mdi absolute top-1/2 -translate-y-1/2 right-4 cursor-pointer"
+        :class="{ 'err-message': error }"
+        :icon="type === 'password' ? 'mdi:eye-outline' : 'mdi:eye-off-outline'"
+        @click="$emit('changeType')"
+      />
+    </div>
+    <small
+      :class="{ 'err-message': error }"
+      v-if="error && errorMessage"
+      aria-live="assertive"
+      :id="`${label}-error`"
+      >{{ errorMessage }}</small
+    >
+    <small
+      :class="{ 'err-message': error }"
+      v-else-if="error && !errorMessage"
+      aria-live="assertive"
+      :id="`${label}-error`"
+      >{{ label }} is required</small
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+interface Props {
+  type: string
+  modelValue: string | number | object
+  label?: string
+  placeholder?: string
+  password?: boolean
+  error?: string | boolean
+  errorMessage?: string | boolean
+}
+
+const props = defineProps<Props>()
+const emit = defineEmits(['changeType', 'update:modelValue'])
+</script>
+
+<style lang="scss" scoped></style>
