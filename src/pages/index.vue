2719b5fb5a82756b833302234817a17dd1ad2e3c
Animashaun Taofiq <animashauntaofiq@yahoo.com>
2023-03-19 22:17:03 +0100

    refactor: completed landing page

---
+++ src/pages/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+useHead({
+  title: 'Academic Counseling Seminar (ACS)',
+  meta: [{ name: 'description', content: 'Academic Counseling Seminar (ACS).' }],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: '/favicon.svg'
+    }
+  ]
+})
+</script>
+
+<template>
+  <TheHeader />
+  <HomeMain />
+  <HomeWaitlist />
+  <HomeAgenda />
+  <HomeNetwork />
+  <HomePartners />
+  <HomeSpeakers />
+  <HomeFaq />
+  <HomeTeam />
+  <TheFooter />
+</template>
